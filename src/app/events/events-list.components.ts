ff57605c9028c7ae3104762257335fb71dd0e7eb
Prasanta Rout <prasant.kumar.rout@gmail.com>
2019-11-23 09:31:25 +0000

    update codes

--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -10,6 +10,7 @@ export class EventListComponent  {
         event = {
             id: 1 ,
             name: 'Angular',
-            date: '20/6/1922'
+            date: '20/6/1922',
+            expiry: '31/12/2020'
         } 
 }
